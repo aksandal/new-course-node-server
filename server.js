c88c56225c6fcdd904fad3c15efe8bf6ff381f57
akshaysandal <akshaysandalak@gmail.com>
2018-07-10 12:14:52 +0530

    Intial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const fs=require('fs');
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear()
+});
+
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+})
+
+app.set('template engine', 'hbs');
+
+
+app.use((req,res,next)=>{
+    res.render('maintainance.hbs');
+})
+app.use(express.static(__dirname + '/public'));
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    var log=`${now}:${req.method} ${req.url}`;
+    console.log(log);
+
+    fs.appendFile('server.log',log+'\n',(err)=>{
+        console.log(err);
+    })
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        welcomeMessage:'What the hell welcomes you',
+        pageTitle:'About Page',
+        getCurrentYear:'2016'
+    });
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+        currentYear:new Date().getFullYear()
+    })
+});
+
+
+
+app.listen(3000, 'localhost', () => {
+    console.log('listening on 3000');
+});
